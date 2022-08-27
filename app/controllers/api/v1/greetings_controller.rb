592f22c76b6009089382b4edd847ccbaaa65966a
ForHemer <foreste.hemerson@gmail.com>
2022-08-26 23:49:16 -0400

    Update greetings_controller

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,4 +1,4 @@
-class GreetingsController < ApplicationController
+class Api::V1::GreetingsController < ApplicationController
   def index
     @greeting = Greeting.find(rand(1..5))
     render json: @greeting
